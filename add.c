f0700a12a36b06dbe2f82da1ae25590f736b3557
SebastianBlandon <blandonodz@gmail.com>
2022-05-18 15:18:57 -0500

    [UPDATED] add.c betty check

--- add.c
+++ add.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
- * _add - stack.
- * @stack: Double pointer to the stack.
- * @line_number: value for include in the stack.
+ * _add - pops stack
+ * @stack:Double pointer to the stack
+ * @line_number: value for includein the stack
  * Return: Nothing.
  */
-
 void _add(sstack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
