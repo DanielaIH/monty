6b9ae3956b4ef5d99bcb17836f9067b1d58866ed
SebastianBlandon <blandonodz@gmail.com>
2022-05-18 15:21:09 -0500

    [UPDATED] swap.c betty check

--- swap.c
+++ swap.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
- * _swap - stack.
- * @stack: Double pointer to the stack.
- * @line_number: value for include in the stack.
+ * _swap - pops stack
+ * @stack:Double pointer to the stack
+ * @line_number: value for includein the stack
  * Return: Nothing.
  */
-
 void _swap(sstack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
