afac486d4bdb54379ab77d287d7352ea8bb41cbe
SebastianBlandon <blandonodz@gmail.com>
2022-05-18 15:20:34 -0500

    [UPDATED] pint.c betty check

--- pint.c
+++ pint.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
- * _pint - stack.
- * @stack: Double pointer to the stack.
- * @line_number: value for include in the stack.
+ * _pint - pops stack
+ * @stack:Double pointer to the stack
+ * @line_number: value for includein the stack
  * Return: Nothing.
  */
-
 void _pint(sstack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
