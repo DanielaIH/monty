17586527c8a318d2f96324de891ce813c8706602
SebastianBlandon <blandonodz@gmail.com>
2022-05-18 15:20:50 -0500

    [UPDATED] pop.c betty check

--- pop.c
+++ pop.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 /**
- * _pop - stack.
- * @stack: Double pointer to the stack.
- * @line_number: value for include in the stack.
+ * _pop - pops stack
+ * @stack:Double pointer to the stack
+ * @line_number: value for includein the stack
  * Return: Nothing.
  */
-
 void _pop(sstack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
