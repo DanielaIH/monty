3e7fa1f5c39407918692161ebf69e21ce76ae247
SebastianBlandon <blandonodz@gmail.com>
2022-05-20 14:17:29 -0700

    [UPDATED] swap.c

--- swap.c
+++ swap.c
@@ -10,12 +10,13 @@ void _swap(sstack_t **stack, unsigned int line_number)
 	sstack_t *swap = NULL;
 	(void)line_number;
 
-	if (*stack)
+	if (*stack && (*stack)->next)
 	{
 		swap = *stack;
 		*stack = (*stack)->next;
 		swap->prev = *stack, swap->next = (*stack)->next;
 		(*stack)->prev = NULL, (*stack)->next = swap;
 	}
-
+	else
+		dprintf(2, "L%d: can't swap, stack too short\n", line_number);
 }
