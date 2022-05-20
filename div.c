6e90004f2bf8113b655ae3fa59538ade27a3d6b3
SebastianBlandon <blandonodz@gmail.com>
2022-05-19 20:14:38 -0700

    [ADDED] sub.c, div.c, mul.c

--- div.c
+++ div.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 /**
- * _add - pops stack
+ * _div - pops stack
  * @stack:Double pointer to the stack
  * @line_number: value for includein the stack
  * Return: Nothing.
  */
-void _add(sstack_t **stack, unsigned int line_number)
+void _div(sstack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 
 	if (*stack && (*stack)->next)
-		printf("%d\n", (*stack)->n + (*stack)->next->n);
+		printf("%d\n", (*stack)->n / (*stack)->next->n);
 }
