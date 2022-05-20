3b58c0a725cb60055761d476fcc85a965bc00407
DanielaIH <4427@holbertonschool.com>
2022-05-20 18:18:29 -0500

    FIX sub.c opcode that subtracts the top element of the stack from the second top element of the stack.

--- sub.c
+++ sub.c
@@ -7,8 +7,17 @@
  */
 void _sub(sstack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
+	int sub = 0;
 
 	if (*stack && (*stack)->next)
-		printf("%d\n", (*stack)->n - (*stack)->next->n);
+	{
+		sub = ((*stack)->next->n - (*stack)->n);
+		_pop(stack, 0);
+		(*stack)->n = sub;
+	}
+	else
+	{
+		STATUS[0] = 'F';
+		dprintf(2, "L%d: can't sub, stack too short\n", line_number);
+	}
 }
