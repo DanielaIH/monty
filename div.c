f9ebb5bf3157e8622b9790eb1eb19c97b9f32a9c
DanielaIH <4427@holbertonschool.com>
2022-05-20 21:48:06 -0500

    ADD div.c function that divides the second top element of the stack by the top element of the stack

--- div.c
+++ div.c
@@ -7,8 +7,23 @@
  */
 void _div(sstack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
+	int div = 0;
 
 	if (*stack && (*stack)->next)
-		printf("%d\n", (*stack)->n / (*stack)->next->n);
+	{
+		if ((*stack)->n == 0)
+		{
+			STATUS[0] = 'F';
+			dprintf(2, "L%d: division by zero\n", line_number);
+			return;
+		}
+		div = ((*stack)->next->n / (*stack)->n);
+		(*stack)->next->n = div;
+		_pop(stack, 0);
+	}
+	else
+	{
+		STATUS[0] = 'F';
+		dprintf(2, "L%d: can't div, stack too short\n", line_number);
+	}
 }
