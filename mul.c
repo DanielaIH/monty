20820e7c57f4d98f4f8717aea8967a39a0e0bf29
DanielaIH <4427@holbertonschool.com>
2022-05-20 21:59:52 -0500

    ADD mul.c function that multiplies the second top element of the stack with the top element of the stack

--- mul.c
+++ mul.c
@@ -7,8 +7,17 @@
  */
 void _mul(sstack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
+	int mul = 0;
 
 	if (*stack && (*stack)->next)
-		printf("%d\n", (*stack)->n * (*stack)->next->n);
+	{
+		mul = ((*stack)->next->n * (*stack)->n);
+		(*stack)->next->n = mul;
+		_pop(stack, 0);
+	}
+	else
+	{
+		STATUS[0] = 'F';
+		dprintf(2, "L%d: can't mul, stack too short\n", line_number);
+	}
 }
